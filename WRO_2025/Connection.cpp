5caac9ec918c04fc32b0ec7c1939faa6e39f6784
Norbert Niering <125464473+Norbert04@users.noreply.github.com>
2024-09-22 00:41:23 +0100

    added implementations of constructor of Connection class

--- WRO_2025/Connection.cpp
+++ WRO_2025/Connection.cpp
@@ -1,6 +1,41 @@
 #include "Connection.h"
 
-Connection::Connection()
+wro::Connection::Connection()
 {
+	if ((fileDescriptor = serialOpen("/dev/ttyUSB0", 115200)) < 0)
+		std::cerr << "unable to open serial\n";
+}
 
+wro::Connection::~Connection()
+{
+	serialClose(fileDescriptor);
+}
+
+void wro::Connection::sendMessage(std::string message)
+{
+	if (valid())
+	{
+
+	}
+}
+
+void wro::Connection::sendDebug(std::string information)
+{
+	if (valid())
+	{
+
+	}
+}
+
+void wro::Connection::sendError(std::string error)
+{
+	if (valid())
+	{
+
+	}
+}
+
+bool wro::Connection::valid()
+{
+	return fileDescriptor >= 0;
 }
