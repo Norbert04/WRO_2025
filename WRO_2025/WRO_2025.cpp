afc527788ec426788f970b7ba5631e85c64a6c6a
Norbert Niering <125464473+Norbert04@users.noreply.github.com>
2024-12-01 17:54:14 +0000

    added some testing code to main function

--- WRO_2025/WRO_2025.cpp
+++ WRO_2025/WRO_2025.cpp
@@ -1,4 +1,16 @@
-﻿int main()
+﻿#include "Movement.h"
+#include "Connection.h"
+
+int main()
 {
+	wro::Connection con{};
+	std::cout << "";
+	con.sendMessage("Hello");
+
+	if (con.waitForNext() == wro::connectionCode::message)
+	{
+		std::cout << con.getMessage() << "\n";
+	}
+
 	return 0;
 }
