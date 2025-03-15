71ff27f300dd625e636ccc6610fdf1ebbeb3449e
Matthew Greiner <118717731+FrozenBirdXD@users.noreply.github.com>
2025-03-15 13:40:07 +0100

    test

--- RPI/Boot.cpp
+++ RPI/Boot.cpp
@@ -3,7 +3,18 @@
 #include <cstdlib>
 #include <iostream>
 
-constexpr int BUTTON_PIN = 0; // GPIO pin (WiringPi pin number, not BCM)
+constexpr int BUTTON_PIN = 27; // GPIO pin (WiringPi pin number, not BCM)
+const char* LED_PATH = "/sys/class/leds/led0/brightness"; 
+
+void setLED(bool state) {
+    std::ofstream ledFile(LED_PATH);
+    if (ledFile) {
+        ledFile << (state ? "1" : "0");
+        ledFile.close();
+    } else {
+        std::cerr << "Failed to access " << LED_PATH << std::endl;
+    }
+}
 
 int main() {
 	if (wiringPiSetup() == -1) {
@@ -20,6 +31,8 @@ int main() {
 			delay(100);
 		}
 #if defined(DEBUG) || defined(_DEBUG)
+setLED(true); // Turn LED on
+
 		std::cout << "Button pressed, starting main" << std::endl;
 #endif // defined(DEBUG) || defined(_DEBUG)
 
