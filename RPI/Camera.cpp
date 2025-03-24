cb00e4034eeece6b932bc1afb51789fa68de05bb
Norbert04 <125464473+Norbert04@users.noreply.github.com>
2025-03-24 21:38:33 +0000

    added files for camera use

---
+++ RPI/Camera.cpp
@@ -0,0 +1,9 @@
+#include "Camera.h"
+
+wro::Camera::Camera()
+{
+}
+
+wro::Camera::~Camera()
+{
+}
---
+++ RPI/Camera.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#include <libcamera/libcamera.h>
+
+namespace wro
+{
+	class Camera
+	{
+	public:
+		Camera();
+		~Camera();
+
+	private:
+	};
+}
