25e5928e9340d9fa7c7323b51aab03d3cb8a9efc
Norbert04 <125464473+Norbert04@users.noreply.github.com>
2025-03-24 22:17:38 +0000

    added code to initialise Connection and call handleEvent method when serial data is received

--- ARDUINO/main/main.ino
+++ ARDUINO/main/main.ino
@@ -1,12 +1,23 @@
+// #define _DEBUG // uncomment for debugging
+
 #include "Connection.h"
 #include "Movement.h"
 
 void setup()
 {
-    
+    wro::EventHandlers handlers;
+    handlers.drive = wro::movement::drive;
+    handlers.steer = wro::movement::steer;
+    handlers.stop = wro::movement::stop;
+    wro::Connection::Get()->setEventHandlers(handlers);
 }
 
 void loop()
 {
     
+}
+
+void serialEvent()
+{
+    wro::Connection::Get()->handleEvent();
 }
