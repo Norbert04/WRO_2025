062292da7b8f3370b4b8ccd61cac20fbe9b16373
Matthew Greiner <118717731+FrozenBirdXD@users.noreply.github.com>
2025-03-31 22:39:02 +0200

    testing

--- ARDUINO/main/main.ino
+++ ARDUINO/main/main.ino
@@ -5,19 +5,26 @@
 
 void setup()
 {
+  pinMode(13, OUTPUT);
     wro::EventHandlers handlers;
-    handlers.drive = wro::movement::drive;
-    handlers.steer = wro::movement::steer;
-    handlers.stop = wro::movement::stop;
+    handlers.drive = &wro::movement::drive;
+    handlers.steer = &wro::movement::steer;
+    handlers.stop = &wro::movement::stop;
     wro::Connection::Get()->setEventHandlers(handlers);
+    //Serial.begin(9600);
+    wro::movement::init();
+    wro::movement::drive(0.6);
 }
 
 void loop()
-{
-    
+{ 
+      if(Serial.available() != 0) {
+        serialEvent();
+      }
 }
 
 void serialEvent()
 {
+    
     wro::Connection::Get()->handleEvent();
 }
