0422d0a30f61a3b0549eb1a2c1e1b3c80dcea52c
Norbert04 <125464473+Norbert04@users.noreply.github.com>
2025-04-27 11:01:53 +0100

    removed Boot.cpp and replaced it with near-equivalent code in WRO_2025.cpp

--- RPI/WRO_2025.cpp
+++ RPI/WRO_2025.cpp
@@ -1,20 +1,38 @@
-﻿#include <wiringPi.h>
+﻿#include <chrono>
+#include <thread>
+
+#include <wiringPi.h>
 
 #include "Camera.h"
 #include "Robot.h"
+#include "utils.h"
 
 int main()
 {
+	constexpr int P_BUTTON_START = 27;
+
 	if (wiringPiSetup() != 0)
 	{
 		std::cerr << "WiringPi setup failed\n";
 		return -1;
 	}
 
-	// wro::Robot robot = wro::Robot();
+	pinMode(P_BUTTON_START, INPUT);
+	pullUpDnControl(P_BUTTON_START, PUD_UP);
 
-	wro::Camera cam{};
-	cam.runTest();
+	wro::Robot robot = wro::Robot();
+
+	while (digitalRead(BUTTON_PIN) == HIGH)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
+
+	DEBUG_PRINTLN("Button pressed, starting execution");
+	{ // main
+
+		wro::Camera cam{};
+		cam.runTest();
+	}
 
 	return 0;
 }
