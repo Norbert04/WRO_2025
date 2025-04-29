0554ac4ca64f024783aabb8328f5b8c1c2f72b50
Norbert04 <125464473+Norbert04@users.noreply.github.com>
2025-04-29 20:31:55 +0100

    switched to bcm pin numbering

--- RPI/WRO_2025.cpp
+++ RPI/WRO_2025.cpp
@@ -11,9 +11,9 @@
 
 int main()
 {
-	constexpr int P_BUTTON_START = 27;
+	constexpr int P_BUTTON_START = 16;
 
-	if (wiringPiSetup() != 0)
+	if (wiringPiSetupGpio() != 0)
 	{
 		std::cerr << "WiringPi setup failed\n";
 		return -1;
